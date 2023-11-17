56980d12f8650884f119895e229e60507ef4e497
Senkyano <91433801+Senkyano@users.noreply.github.com>
2023-11-18 00:14:20 +0100

    Add files via upload

---
+++ ft_printf/libftprintf_R.h
@@ -0,0 +1,12 @@
+#ifndef _LIBFTPRINTF_R_H
+# define _LIBFTPRINTF_R_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+void	print_char(char c);
+int		print_str(char *str);
+
+#endif
