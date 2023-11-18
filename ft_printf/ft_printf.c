4b4f8fb02959a9229d7c03d81176da891098a02f
Senkyano <91433801+Senkyano@users.noreply.github.com>
2023-11-18 15:27:32 +0100

    finish need to debug

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+#include "libftprintf_R.h"
 
 int	ft_printf(const char *str, ...)
 {
@@ -37,6 +37,16 @@ int	ft_printf(const char *str, ...)
 	return (lenght);
 }
 
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
 int	same_letter(char c, const char *str)
 {
 	while (*str != '\0')
@@ -54,9 +64,23 @@ int	lenght_output(char c, va_list args)
 
 	lenght = 0;
 	if (c == 'c')
-		lenght = print_char(va_args(args, int));
+		lenght = print_char(va_arg(args, int));
 	else if (c == 's')
-		lenght = print_str(char *);
+		lenght = print_str(va_arg(args, char *));
+	else if (c == 'p')
+		lenght = print_adr(va_arg(args, void *), "0123456789abcdef");
+	else if (c == 'd')
+		lenght = print_base(va_arg(args, int), "0123456789");
+	else if (c == 'i')
+		lenght = print_inte(va_arg(args, char *), "0123456789");
+	else if (c == 'u')
+		lenght = print_unsnumber(va_arg(args, unsigned int));
+	else if (c == 'x')
+		lenght = print_base(va_arg(args, int), "0123456789abcdef");
+	else if (c == 'X')
+		lenght = print_base(va_arg(args, int), "0123456789ABCDEF");
+	else
+		lenght = print_percent();
 	return (lenght);
 }
 
@@ -85,44 +109,69 @@ int	print_str(char *str)
 /* ************************************************************************** */
 /*                               %p                                           */
 /* ************************************************************************** */
-int	
-
-/* ************************************************************************** */
-/*                               %d                                           */
-/* ************************************************************************** */
-int	print_number(int n)
+int	print_adr(long unsigned int ptr, const char *base)
 {
-	long int nbr;
 	int	lenght;
 
-	nbr = n;
 	lenght = 0;
-	if (nbr < 0)
-	{
-		lenght += print_char('-');
-		nbr = -nbr;
-	}
-	if (nbr > 9)
-		lenght += print_number(nbr / 10);
-	lenght += print_char(nbr % 10);
+	lenght += print_char('0');
+	lenght += print_char('x');
+	if (ptr > ft_strlen(base))
+		lenght += print_adr(ptr / ft_strlen(base), base);
+	lenght += print_char(base[ptr % ft_strlen(base)]);
 	return (lenght);
 }
 
 /* ************************************************************************** */
 /*                               %i                                           */
 /* ************************************************************************** */
+int	print_inte(char *hexa, const char *base)
+{
+	int	nbr;
+	int	n;
 
+	n = 1;
+	nbr = 0;
+	if (*hexa == '-')
+	{
+		n = -1;
+		hexa++;
+	}
+	while (*hexa != '\0' && cible(*hexa, base) != -1)
+	{
+		nbr = nbr * ft_strlen(base) + cible(*hexa, base);
+		hexa++;
+	}
+	nbr *= n;
+	return (print_base(nbr, base));
+}
+
+int	cible(char c, const char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (c == base[i])
+			return (i);
+		i++;
+	}
+	return (-1);
+}
 /* ************************************************************************** */
 /*                               %u                                           */
 /* ************************************************************************** */
+int	print_unsnumber(unsigned int nbr)
+{
+	int	lenght;
 
-/* ************************************************************************** */
-/*                               %x                                           */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/*                               %X                                           */
-/* ************************************************************************** */
+	lenght = 0;
+	if (nbr > 9)
+		lenght += print_unsnumber(nbr / 10);
+	lenght += print_char((nbr % 10) + '0');
+	return (lenght);
+}
 
 /* ************************************************************************** */
 /*                               %%                                           */
@@ -130,4 +179,26 @@ int	print_number(int n)
 int	print_percent(void)
 {
 	return (print_char('%'));
+}
+
+/* ************************************************************************** */
+/*                  %x        et        %X        et        %d                */
+/* ************************************************************************** */
+
+int	print_base(int n, const char *base)
+{
+	long int nbr;
+	int	lenght;
+
+	lenght = 0;
+	nbr = n;
+	if (nbr < 0)
+	{
+		lenght += print_char('-');
+		nbr = -nbr;
+	}
+	if (nbr >= ft_strlen(base))
+		lenght += print_base(nbr / ft_strlen(base), base);
+	lenght += print_char(base[nbr % ft_strlen(base)]);
+	return (lenght);
 }
--- ft_printf/libftprintf_R.h
+++ ft_printf/libftprintf_R.h
@@ -6,7 +6,17 @@
 #include <stdarg.h>
 #include <string.h>
 
-void	print_char(char c);
+int		print_char(char c);
 int		print_str(char *str);
+int		ft_printf(const char *str, ...);
+int		same_letter(char c, const char *str);
+int		lenght_output(char c, va_list args);
+int		print_adr(long unsigned int ptr, const char *base);
+int		print_inte(char *hexa, const char *base);
+int		cible(char c, const char *base);
+int		print_unsnumber(unsigned int nbr);
+int		print_percent(void);
+int		print_base(int n, const char *base);
+int		ft_strlen(const char *str);
 
 #endif
