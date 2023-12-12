960eae6ff011313406d52e9118b78ace2788d31c
Senkyano <91433801+Senkyano@users.noreply.github.com>
2023-12-12 17:49:33 +0100

    name of variable change

--- ft_printf.c
+++ ft_printf.c
@@ -46,7 +46,7 @@ int	ft_output(char c, va_list args, int length)
 	if (c == 'c')
 		length = ft_print_char(va_arg(args, int));
 	else if (c == 's')
-		lenght = ft_print_str(va_arg(args, char *));
+		length = ft_print_str(va_arg(args, char *));
 	else if (c == 'p')
 		length = ft_addr(va_arg(args, uintptr_t), "0123456789abcdef");
 	else if (c == 'd' || c == 'i')
